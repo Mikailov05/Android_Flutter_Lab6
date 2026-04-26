3451746f024d2b46b4a10452e42a60f8cb2680d1
Mikailov05 <ahmed.mikailov@interner.ru>
2026-04-26 22:22:17 +0300

    Bonus: add sound effects via Web Audio API

--- lib/slot_machine.dart
+++ lib/slot_machine.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
 import 'package:slot_machine/slot_row.dart';
+import 'sound_service.dart';
 
 class SlotMachine extends StatefulWidget {
   const SlotMachine({super.key});
@@ -22,6 +23,14 @@ class _SlotMachineState extends State<SlotMachine> {
   var _slot3 = 'assets/images/seven.jpg';
   var _message = '';
   var _isSpinning = false;
+  var _isMuted = false;
+  var _backgroundStarted = false;
+
+  @override
+  void initState() {
+    super.initState();
+    SoundService.playBackground();
+  }
 
   Future<String> _spinReel({
     required int totalTicks,
@@ -44,10 +53,15 @@ class _SlotMachineState extends State<SlotMachine> {
 
   Future<void> _spin() async {
     if (_coins <= 0 || _isSpinning) return;
+    SoundService.playClick();
     setState(() {
       _isSpinning = true;
       _message = '';
     });
+    if (!_backgroundStarted) {
+      SoundService.playBackground();
+      _backgroundStarted = true;
+    }
     final result1 = await _spinReel(
       totalTicks: 10,
       onTick: (val) => setState(() => _slot1 = val),
@@ -66,14 +80,17 @@ class _SlotMachineState extends State<SlotMachine> {
       if (result1 == result2 && result2 == result3) {
         if (result1 == 'assets/images/seven.png') {
           _coins += 10;
-          _message = 'ДЖЕКПОТ! 🎉🎉🎉 +10 монет';
+          _message = 'ДЖЕКПОТ! 🎉🎉🎉🎉 +10 монет';
+          SoundService.playJackpot();
         } else {
           _coins += 3;
-          _message = 'Победа! 🎉🎉🎉 +3 монеты';
+          _message = 'Победа! 🎉🎉🎉🎉 +3 монеты';
+          SoundService.playWin();
         }
       } else {
         _coins -= 1;
-        _message = 'Попробуй ещё раз 😊 -1 монета';
+        _message = 'Попробуй ещё раз 😂 -1 монета';
+        SoundService.playLose();
       }
     });
   }
@@ -89,11 +106,34 @@ class _SlotMachineState extends State<SlotMachine> {
     });
   }
 
+  void _toggleMute() {
+    SoundService.toggleMute();
+    setState(() {
+      _isMuted = SoundService.isMuted;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
+        Align(
+          alignment: Alignment.topRight,
+          child: Padding(
+            padding: EdgeInsets.only(right: 16, top: 8),
+            child: IconButton(
+              onPressed: _toggleMute,
+              icon: Icon(
+                _isMuted
+                    ? Icons.volume_off
+                    : Icons.volume_up,
+                color: Colors.white,
+                size: 28,
+              ),
+            ),
+          ),
+        ),
         Text(
           'Монеты: $_coins',
           style: TextStyle(
